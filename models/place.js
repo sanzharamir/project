7b7a14b8a675761bef4d922d9ce93830cc6d4b37
Sanzhar <sanzhar.amirgaliyev@gmail.com>
2020-08-07 10:06:18 +0000

    add created time

--- models/place.js
+++ models/place.js
@@ -5,6 +5,7 @@ var placeSchema = new mongoose.Schema({
 	price: String,
 	image: String,
 	description: String,
+	createdAt: {type: Date, default: Date.now},
 	author: {
 		id:	{
         	type: mongoose.Schema.Types.ObjectId,
